c90dc59e118cea9b220eddf2284fbcea5fa9decc
jiani <jiani@pc-longjiani>
2015-04-25 16:24:36 +0200

    Création classe sceance

---
+++ Projet_GestionPlanning/src/model/Sceance.java
@@ -0,0 +1,39 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package model;
+
+import model.Module;
+import model.Jour;
+
+/**
+ *
+ * @author jiani
+ */
+public class Sceance {
+
+    private Jour jourSceance;
+    private Module leModule;
+
+    public Sceance(Jour jour, Module mod) {
+        this.jourSceance = jour;
+        this.leModule = mod;
+    }
+
+    public Jour getJourSceance() {
+        return jourSceance;
+    }
+
+    public Module getLeModule() {
+        return leModule;
+    }
+
+    public void setJourSceance(Jour jour) {
+        this.jourSceance = jour;
+    }
+
+    public void setLeModule(Module mod) {
+        this.leModule = mod;
+    }
+}
