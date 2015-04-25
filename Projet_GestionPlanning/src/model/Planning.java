9cff333df978f013d2af5e4020c193fa817b7727
jiani <jiani@pc-longjiani>
2015-04-25 16:24:01 +0200

    Création classe planning

---
+++ Projet_GestionPlanning/src/model/Planning.java
@@ -0,0 +1,44 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package model;
+
+import java.util.ArrayList;
+import modele.Semaine;
+
+/**
+ *
+ * @author Maïlys
+ */
+public class Planning {
+    
+    private ArrayList<Semaine>lesSemaines;
+    private Planning laPromotion;
+    private int annee;
+    
+    public Planning (ArrayList<Semaine>lesSemaines, Planning promotion, int annee){
+        this.lesSemaines = lesSemaines;
+        this.laPromotion = laPromotion;
+        this.annee = annee;
+    }
+    public ArrayList getLesSemaines(){
+        return lesSemaines;
+    }
+    public void setLesSemaine (ArrayList<Semaine>lesSemaines){
+        this.lesSemaines= lesSemaines;
+    }
+    public Planning getLaPromotion(){
+        return laPromotion;
+    }
+    public void laPromotion(Planning laPromotion){
+        this.laPromotion = laPromotion;
+    }
+    public int getAnnee (){
+        return annee;
+    }
+    public void setAnnee(int annee){
+        this.annee = annee;
+    }
+}
