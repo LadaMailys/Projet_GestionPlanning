63eb02407382784dd9a3d41030ab0d3b3eec9232
jiani <jiani@pc-longjiani>
2015-04-25 16:20:54 +0200

    Création classe Jour

---
+++ Projet_GestionPlanning/src/model/Jour.java
@@ -0,0 +1,69 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package model;
+
+/**
+ *
+ * @author Maïlys
+ */
+public class Jour {
+    
+    private char moment;
+    private String jour;
+    private int dateJour;
+    private int mois;
+    private int annee;
+    private boolean ouvre;
+    
+    public Jour (String jour, int dateJour, int mois, int annee){
+        this.jour=jour;
+        this.dateJour=dateJour;
+        this.mois=mois;
+        this.annee=annee;
+        if (this.jour.toUpperCase().equals("SAMEDI") || this.jour.toUpperCase().equals("DIMANCHE"))
+            ouvre=true;
+        else
+            ouvre = false;
+    }
+    
+    public char getMoment(){
+        return moment;
+    }
+    public void setMoment(char moment){
+        this.moment = moment;
+    }
+    
+    public String getJour(){
+        return jour;
+    }
+    public void setJour(String jour){
+        this.jour =  jour;
+    }
+    public int getDateJour (){
+        return dateJour;
+    }
+    public void setDateJour (int dateJour){
+        this.dateJour = dateJour;
+    }
+    public int getMois(){
+        return mois;
+    }
+    public void setMois (int mois){
+        this.mois = mois;
+    }
+    public int getAnnee (){
+        return annee;
+    }
+    public void setAnnee (int annee){
+        this.annee = annee;
+    }
+    public boolean getOuvre(){
+        return ouvre;
+    }
+    public void setOuvre(boolean ouvre){
+        this.ouvre = ouvre;
+    }
+} 
