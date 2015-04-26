8f68234bb7e3215afe57e2025a3a91c9f0f0e755
Nephilder <cynthanger@gmail.com>
2015-04-26 15:00:52 +0200

    Optimisation code

--- Projet_GestionPlanning/src/model/Jour.java
+++ Projet_GestionPlanning/src/model/Jour.java
@@ -10,60 +10,67 @@ package model;
  * @author Maïlys
  */
 public class Jour {
-    
+
     private char moment;
     private String jour;
     private int dateJour;
     private int mois;
     private int annee;
     private boolean ouvre;
-    
-    public Jour (String jour, int dateJour, int mois, int annee){
-        this.jour=jour;
-        this.dateJour=dateJour;
-        this.mois=mois;
-        this.annee=annee;
-        if (this.jour.toUpperCase().equals("SAMEDI") || this.jour.toUpperCase().equals("DIMANCHE"))
-            ouvre=true;
-        else
-            ouvre = false;
+
+    public Jour(String jour, int dateJour, int mois, int annee) {
+        this.jour = jour;
+        this.dateJour = dateJour;
+        this.mois = mois;
+        this.annee = annee;
+        this.ouvre = this.jour.toUpperCase().equals("SAMEDI") || this.jour.toUpperCase().equals("DIMANCHE");
     }
-    
-    public char getMoment(){
+
+    public char getMoment() {
         return moment;
     }
-    public void setMoment(char moment){
+
+    public void setMoment(char moment) {
         this.moment = moment;
     }
-    
-    public String getJour(){
+
+    public String getJour() {
         return jour;
     }
-    public void setJour(String jour){
-        this.jour =  jour;
+
+    public void setJour(String jour) {
+        this.jour = jour;
     }
-    public int getDateJour (){
+
+    public int getDateJour() {
         return dateJour;
     }
-    public void setDateJour (int dateJour){
+
+    public void setDateJour(int dateJour) {
         this.dateJour = dateJour;
     }
-    public int getMois(){
+
+    public int getMois() {
         return mois;
     }
-    public void setMois (int mois){
+
+    public void setMois(int mois) {
         this.mois = mois;
     }
-    public int getAnnee (){
+
+    public int getAnnee() {
         return annee;
     }
-    public void setAnnee (int annee){
+
+    public void setAnnee(int annee) {
         this.annee = annee;
     }
-    public boolean getOuvre(){
+
+    public boolean isOuvre() {
         return ouvre;
     }
-    public void setOuvre(boolean ouvre){
+
+    public void setOuvre(boolean ouvre) {
         this.ouvre = ouvre;
     }
-} 
+}
