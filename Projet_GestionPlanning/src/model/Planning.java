4c364203e9b91acc975e4252107b7e0a720c0244
u21405875 <u21405875@WIN097.auth.local>
2015-05-09 12:30:14 +0200

    Ajout nouvelle liste "SemaineSoir" + "SemaineMatin"

--- Projet_GestionPlanning/src/model/Planning.java
+++ Projet_GestionPlanning/src/model/Planning.java
@@ -16,7 +16,8 @@ import java.util.concurrent.TimeUnit;
  */
 public class Planning {
 
-    private HashMap<Integer, ArrayList<Jour>> lesSemaines;
+    private HashMap<Integer, ArrayList<Jour>> lesSemainesMatin;
+    private HashMap<Integer, ArrayList<Jour>> lesSemainesSoir;
     private Promotion laPromotion;
     private int annee;
 
@@ -26,22 +27,28 @@ public class Planning {
     }
 
     public Planning(Promotion promotion, int annee) {
-        this.lesSemaines = new HashMap<>();
+        this.lesSemainesMatin = new HashMap<>();
+        this.lesSemainesSoir = new HashMap<>();
         this.laPromotion = promotion;
         this.annee = annee;
         remplirCalendrier();
     }
-    
-    public int getNbSemainesAnnee(){
-        return lesSemaines.size();
+
+    public int getNbSemainesAnnee() {
+        return lesSemainesMatin.size();
     }
 
-    public ArrayList<Jour> getLaSemaine(int rang) {
-        return lesSemaines.get(rang);
+    public ArrayList<Jour> getLaSemaineMatin(int rang) {
+        return lesSemainesMatin.get(rang);
+    }
+
+    public ArrayList<Jour> getLaSemaineSoir(int rang) {
+        return lesSemainesSoir.get(rang);
     }
 
     public void ajouteSemaine(int rang, ArrayList<Jour> jours) {
-        this.lesSemaines.put(rang, jours);
+        this.lesSemainesMatin.put(rang, jours);
+        this.lesSemainesSoir.put(rang, jours);
     }
 
     public Promotion getLaPromotion() {
@@ -63,18 +70,18 @@ public class Planning {
     /**
      * Réinitialise le HashMap lesSemaines<int rang, ArrayList<Jour>> lesJours
      */
-    public final void reinitCalendrier(){
-        this.lesSemaines.clear();
+    public final void reinitCalendrier() {
+        this.lesSemainesMatin.clear();
+        this.lesSemainesSoir.clear();
         remplirCalendrier();
     }
-    
+
     /**
-     * Méthode qui remplit automatiquement le HashMap lesSemaines<int rang, ArrayList<Jour>> lesJours
-     * à partir de l'année du planning
+     * Méthode qui remplit automatiquement le HashMap lesSemaines<int rang,
+     * ArrayList<Jour>> lesJours à partir de l'année du planning
      */
     public final void remplirCalendrier() {
         try {
-            
             ArrayList<Jour> lesJours = null;
             Calendar calDeb = Calendar.getInstance();
             Calendar calFin = Calendar.getInstance();
@@ -86,25 +93,25 @@ public class Planning {
             Jour j;
             int compt = -1;
             for (int i = 1; i <= nbJours; i++) {
+                
                 // Création du Jour j avec les données du Calendrier cal
-                j = new Jour();
-                j.setAnnee(calDeb.get(Calendar.YEAR));
-                j.setMois(calDeb.get(Calendar.MONTH) + 1);
-                j.setDateJour(calDeb.get(Calendar.DAY_OF_MONTH));
-                j.setJour(rangJourToString(calDeb.get(Calendar.DAY_OF_WEEK)));
+                j = new Jour(rangJourToString(calDeb.get(Calendar.DAY_OF_WEEK)), 
+                        calDeb.get(Calendar.DAY_OF_MONTH), 
+                        calDeb.get(Calendar.MONTH) + 1, 
+                        calDeb.get(Calendar.YEAR));
 
                 // On remplit la liste à partir de lundi
                 if (j.getJour().equals("Lundi")) {
                     lesJours = new ArrayList<>();
                     compt = 6;
                 }
-                
+
                 // Si le compteur vaut 0 ou plus, c'est que dans la boucle, on est entre lundi et dimanche
                 if (compt >= 0) {
                     lesJours.add(j);
                     compt--;
                 }
-                
+
                 // Si le compteur vaut -1, on vient de rajouter le dimanche dans la liste => Fin de liste
                 // Ou bien c'est la première itération, qui n'est pas un lundi, dans ces cas on ne fait rien
                 if (compt == -1 && lesJours != null) {
@@ -119,10 +126,16 @@ public class Planning {
                 calDeb.add(Calendar.DATE, 1);
             }
         } catch (Exception e) {
-            javax.swing.JOptionPane.showMessageDialog(null,e.getMessage());
+            javax.swing.JOptionPane.showMessageDialog(null, e.getMessage());
         }
     }
 
+    /**
+     * Retourne le jour de la semaine en toute lettre
+     * selon le rang donné
+     * @param rang
+     * @return String
+     */
     public String rangJourToString(int rang) {
         switch (rang) {
             case 1:
