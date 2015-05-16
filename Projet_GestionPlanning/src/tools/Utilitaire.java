987929f576f1edbade1ef2ba331b317345e613e8
u21405875 <u21405875@WIN112.auth.local>
2015-05-16 16:04:52 +0200

    Création classe Utilitaire > Méthode isInteger(Objet obj)

---
+++ Projet_GestionPlanning/src/tools/Utilitaire.java
@@ -0,0 +1,23 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package tools;
+
+/**
+ *
+ * @author u21405875
+ */
+public class Utilitaire {
+
+    public boolean isInteger(Object obj){
+        try{
+            Integer.parseInt(obj.toString());
+            return true;
+        } catch (Exception e){
+            return false;
+        }
+    }
+
+}
