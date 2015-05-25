e4034d6be048a17868ae77c07193555c306395fe
Nephilder <cynthanger@gmail.com>
2015-05-25 22:25:50 +0200

    Ajout méthodes pour retrouver objet a partir d'un libelle Ajout image

--- Projet_GestionPlanning/src/tools/Utilitaire.java
+++ Projet_GestionPlanning/src/tools/Utilitaire.java
@@ -5,15 +5,16 @@ package tools;
  * To change this template file, choose Tools | Templates
  * and open the template in the editor.
  */
-
-
 import java.util.regex.Pattern;
+import model.*;
 
 /**
  *
  * @author u21405875
  */
 public class Utilitaire {
+
+    static Sauvegarde s = Sauvegarde.deserialiser();;
 
     /**
      * Définit si l'objet passé en paramètre est de convertissable en Integer
@@ -29,18 +30,51 @@ public class Utilitaire {
             return false;
         }
     }
-    
+
     /**
      * Détermine si le String passé en paramètre ne contient que des lettres
+     *
      * @param s
-     * @return 
+     * @return
      */
     public static boolean isAlpha(String s) {
-        if (!(Pattern.matches("^[a-zA-Z]+$", s.trim()))) 
+        if (!(Pattern.matches("^[a-zA-Z]+$", s.trim()))) {
             return false;
-         else 
+        } else {
             return true;
-        
+        }
+
+    }
+
+    /**
+     * Retourne le module par rapport au nom passé en paramètre
+     * et à la sauvegarde en cours
+     * @param nom
+     * @param sa
+     * @return 
+     */
+    public static Module getModule(String nom, Sauvegarde sa) {
+        for (Module m : sa.getLstModules()) {
+            if (m.getNom().equals(nom)) {
+                return m;
+            }
+        }
+        return null;
+    }
+
+    /**
+     * Retourne la promotion par rapport au nom passé en paramètre
+     * et à la sauvegarde en cours
+     * @param nom
+     * @return 
+     */
+    public static Promotion getPromotion(String nom) {
+        for (Promotion p : s.getLstPromotions()) {
+            if (p.getNom().equals(nom)) {
+                return p;
+            }
+        }
+        return null;
     }
 
 }
